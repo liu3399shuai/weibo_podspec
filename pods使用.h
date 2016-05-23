2bdf5ca44ccf106bd2fb141529c2660fd2bc6917
liushuai <liu3399shuai@163.com>
2016-05-23 15:58:09 +0800

    user

---
+++ pods使用.h
@@ -0,0 +1,15 @@
+/*
+
+ 
+ update命令  pod update --no-repo-update
+ 
+ 微博SDK现在不支持 pods 1.0.0 ，所以
+ 
+ 更改pod如下
+ 
+ pod 'WeiboSDK', :podspec => 'https://raw.githubusercontent.com/liu3399shuai/weibo_podspec/master/WeiboSDK.podspec'
+ 
+ 这样可以临时解决下
+ 
+
+*/
